2cef7cfa63a4cc95881af5a8b76fad19f7c6bd7a
Marco Lau <lau.career@gmail.com>
2014-06-21 14:37:19 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   has_secure_password
   before_save { email.downcase! }
   before_save { self.email = email.downcase }
@@ -8,4 +11,18 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
